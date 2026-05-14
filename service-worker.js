255dbe3118f4461cfd03d4742cddaa8b7b6b528a
Lucas Ruon <lucasruon@MacBook-Pro.local>
2026-05-14 16:24:13 -0300

    ajustes gerais 3

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'diamondx-v26';
+const CACHE_NAME = 'diamondx-v27';
 const ASSETS = [
     '/',
     '/index.html',
     '/manifest.json?v=3',
-    '/css/reset.css?v=6',
+    '/css/reset.css?v=8',
     '/css/variables.css',
-    '/css/components.css?v=18',
+    '/css/components.css?v=19',
     '/css/pages.css?v=2',
     '/js/app.js?v=18',
     '/js/auth.js',
